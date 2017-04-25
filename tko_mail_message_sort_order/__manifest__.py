9bc42ceaee9527a6665b766100ad35a51a723ec0
zimbora <alexandre.ruffer@tkobr.com>
2017-04-25 20:16:58 +0100

    Update __manifest__.py

--- tko_mail_message_sort_order/__manifest__.py
+++ tko_mail_message_sort_order/__manifest__.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
-# © 2016-17 Geminate Consultancy Services
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#    Thinkopen - Brasil
+#    Copyright (C) Thinkopen Solutions (<http://www.thinkopensolutions.com.br>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     'name'      : 'Tko Mail Message Sort Order',
