ff1ed39d0530819cfaf2ff4ec1d8ba1b3bba2c8c
purnendu03 <purnendusingh1985@gmail.com>
2017-05-02 18:18:11 +0530

    [ADD] Downgrade message shoring feaute for v9

---
+++ tko_mail_message_sort_order/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2016-17 Geminate Consultancy Services
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name'      : 'Tko Mail Message Sort Order',
+    'version'   : '1.0',
+    'category'  : 'mail',
+    'license'   : 'AGPL-3', 
+    'author'    : 'Geminate Consultancy Services',
+    'website'   : 'www.geminatecs.com',
+    'depends'   :  ['mail'],
+    'data'      :  ['views/mail_template.xml'],
+    
+    'installable': True,
+    'auto_install': False,
+    'application': True
+}
